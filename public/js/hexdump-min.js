d9259ac7c77d2bcacfaef9cd9d3bcde6c3bde51b
Dustin Willis Webber <dustin.webber@gmail.com>
2011-07-28 21:16:32 -0400

    update

--- public/js/hexdump-min.js
+++ public/js/hexdump-min.js
@@ -1,4 +1,4 @@
-// Hexdump.js 0.1.0
+// Hexdump.js 0.1.1
 // (c) 2011 Dustin Willis Webber
 // Hexdump is freely distributable under the MIT license.
 // For all details and documentation:
@@ -13,5 +13,5 @@ this.options.style.hexLeft;for(a=0;a<this.hexdump[c].data.length;a++)b==this.opt
 c.string;this.output+=this.options.style.stringRight};f.prototype.splitNulls=function(c){var b=[],a="";if(c&&c.length>2)for(var d=0;d<c.length;d++)(d+1)%2==0?(a+=c[d].toString(),b.push(a),a=""):a+=c[d].toString();return b};f.prototype.process=function(c){for(var b=[],a=[],d=0;d<c.length;d++){if(this.options.html){var e=this.baseConvert(c[d]);if(this.hex){e=this.splitNulls(e);for(var f=0;f<e.length;f++)a.push('<span data-hex-id="'+this.hexCounter+'">'+e[f]+"</span>")}else a.push('<span data-hex-id="'+
 this.hexCounter+'">'+e+"</span>");b.push('<span data-string-id="'+this.hexCounter+'">'+this.checkForNonPrintable(c[d])+"</span>")}else{e=this.baseConvert(c[d]);if(this.hex){e=this.splitNulls(e);for(f=0;f<e.length;f++)a.push(e[f])}else a.push(e);b.push(this.checkForNonPrintable(c[d]))}this.hexCounter++}d=this.hex?this.options.width*2:this.options.width;if(a.length<d){c=d-a.length;for(d=0;d<c;d++)e="",e=this.options.html?'<span data-hex-null="true">'+this.options.style.hexNull+"</span>":this.options.style.hexNull,
 a.push(e)}if(b.length<this.options.width){c=this.options.width-b.length;for(d=0;d<c;d++)e="",e=this.options.html?'<span data-string-null="true">'+this.options.style.stringNull+"</span>":this.options.style.stringNull,b.push(e)}return{data:a,string:b.join("")}};f.prototype.setNullPadding=function(c){var b=this.options.style.hexNull[0];this.options.style.hexNull="";this.hex&&(c/=2);for(var a=0;a<c;a++)this.options.style.hexNull+=b};f.prototype.addPadding=function(c,b){for(var a=c.toString().length,d=
-"",e=0;e<b-a;e++)d+="0";return this.options.endian=="big"?d+c:c+d};f.prototype.checkForNonPrintable=function(c){var b=c.charCodeAt(0).toString(16);return b==9?".":b==127?".":b.length>2?".":c};return f}();
+"",e=0;e<b-a;e++)d+="0";return this.options.endian=="big"?d+c:c+d};f.prototype.checkForNonPrintable=function(c){var b=c.charCodeAt(0).toString(16);return b==9?".":b==127?".":b.length>2&&this.options.ascii?".":c};return f}();
 
